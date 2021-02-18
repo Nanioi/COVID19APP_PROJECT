435daac513a5f2b6a6c54219a43cde66860fc791
Nanioi <0802ny@gmail.com>
2021-02-19 00:38:39 +0900

    Domestic Activity UI 생성
    - ui 틀 생성
    - 차트 설정 및 ui 생성

--- PracticeProject/app/src/main/java/com/nanioi/practiceproject/DomesticStatusActivity.kt
+++ PracticeProject/app/src/main/java/com/nanioi/practiceproject/DomesticStatusActivity.kt
@@ -1,11 +1,30 @@
 package com.nanioi.practiceproject
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
+import android.graphics.Color
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.github.mikephil.charting.charts.LineChart
+import kotlinx.android.synthetic.main.activity_domestic_status.*
+
 
 class DomesticStatusActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_domestic_status)
+
+        chart.setDrawGridBackground(true)
+        chart.setBackgroundColor(Color.BLACK)
+        chart.setGridBackgroundColor(Color.BLACK)
+
+        summary_btn.setOnClickListener {
+            startActivity(Intent(this,SummaryActivity::class.java))
+        }
+        overseas_btn.setOnClickListener {
+            startActivity(Intent(this,OverseasStatusActivity::class.java))
+        }
+        precaution_btn.setOnClickListener {
+            startActivity(Intent(this, PrecautionActivity::class.java))
+        }
     }
 }
