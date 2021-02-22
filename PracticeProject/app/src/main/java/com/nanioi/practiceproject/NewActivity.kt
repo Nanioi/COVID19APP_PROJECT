57ad28e65bd83a49f893bca6579dea410650357d
Nanioi <0802ny@gmail.com>
2021-02-22 22:26:24 +0900

    xml 파싱, 뷰그리기
    - 다른 방법으로 xml 파싱해 List로 data받음
    - data 로 view그리기

--- PracticeProject/app/src/main/java/com/nanioi/practiceproject/NewActivity.kt
+++ PracticeProject/app/src/main/java/com/nanioi/practiceproject/NewActivity.kt
@@ -3,12 +3,12 @@ package com.nanioi.practiceproject
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import kotlinx.android.synthetic.main.activity_overseas_status_acticity.*
+import kotlinx.android.synthetic.main.activity_new.*
 
 class NewActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_overseas_status_acticity)
+        setContentView(R.layout.activity_new)
 
         summary_btn.setOnClickListener {
             startActivity(Intent(this,SummaryActivity::class.java))
@@ -19,5 +19,6 @@ class NewActivity : AppCompatActivity() {
         precaution_btn.setOnClickListener {
             startActivity(Intent(this, PrecautionActivity::class.java))
         }
+        XmlParsingTask(decide,decide_new,exam,exam_new,clear,clear_new,death,death_new,state_dt,state_time,accumulate_chart).execute()
     }
 }
