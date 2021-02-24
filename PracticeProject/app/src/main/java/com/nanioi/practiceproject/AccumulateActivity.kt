8dae10659d6ffba7b95f0e2f721e5fe9b35e5002
Nanioi <0802ny@gmail.com>
2021-02-24 22:26:49 +0900

    Revert "refactoring"
    This reverts commit 0f8c539b32bd01cafcd0e90c0d0f994d65893247.

--- PracticeProject/app/src/main/java/com/nanioi/practiceproject/AccumulateActivity.kt
+++ PracticeProject/app/src/main/java/com/nanioi/practiceproject/AccumulateActivity.kt
@@ -33,34 +33,46 @@ class AccumulateActivity : AppCompatActivity() {
         precaution_btn.setOnClickListener {
             startActivity(Intent(this, PrecautionActivity::class.java))
         }
-        XmlParsingTask().execute()
+        XmlParsingTask(decide,decide_new,exam,exam_new,clear,clear_new,death,death_new,state_dt,state_time,accumulate_chart).execute()
     }
-    inner class XmlParsingTask() : AsyncTask<Any?, Any?, List<ResponseElement>>() {
-        override fun onPostExecute(result: List<ResponseElement>) {
-            super.onPostExecute(result)
+}
+class XmlParsingTask(
+        val decide: TextView,
+        val decide_new: TextView,
+        val exam: TextView,
+        val exam_new: TextView,
+        val clear: TextView,
+        val clear_new: TextView,
+        val death: TextView,
+        val death_new: TextView,
+        val state_dt: TextView,
+        val state_time: TextView,
+        val chart: LineChart
+) : AsyncTask<Any?, Any?, List<ResponseElement>>() {
+    override fun onPostExecute(result: List<ResponseElement>) {
+        super.onPostExecute(result)
 
-            val data : ResponseElement = result[0]
-            val data_prev : ResponseElement = result[1]
-            val decide_new_cnt  = ((data.decideCnt!!).toInt() - (data_prev.decideCnt!!).toInt()).toString()
-            val exam_new_cnt  = ((data.examCnt!!).toInt() - (data_prev.examCnt!!).toInt()).toString()
-            val clear_new_cnt  = ((data.clearCnt!!).toInt() - (data_prev.clearCnt!!).toInt()).toString()
-            val death_new_cnt  = ((data.deathCnt!!).toInt() - (data_prev.deathCnt!!).toInt()).toString()
+        val data : ResponseElement = result[0]
+        val data_prev : ResponseElement = result[1]
+        val decide_new_cnt  = ((data.decideCnt!!).toInt() - (data_prev.decideCnt!!).toInt()).toString()
+        val exam_new_cnt  = ((data.examCnt!!).toInt() - (data_prev.examCnt!!).toInt()).toString()
+        val clear_new_cnt  = ((data.clearCnt!!).toInt() - (data_prev.clearCnt!!).toInt()).toString()
+        val death_new_cnt  = ((data.deathCnt!!).toInt() - (data_prev.deathCnt!!).toInt()).toString()
 
-            decide.setText(data.decideCnt)
-            decide_new.setText("+" + decide_new_cnt)
-            exam.setText(data.examCnt)
-            exam_new.setText(exam_new_cnt)
-            clear.setText(data.clearCnt)
-            clear_new.setText("+" + clear_new_cnt)
-            death.setText(data.deathCnt)
-            death_new.setText("+" + death_new_cnt)
-            state_dt.setText(data.stateDt)
-            state_time.setText(data.stateTime)
+        decide.setText(data.decideCnt)
+        decide_new.setText("+" + decide_new_cnt)
+        exam.setText(data.examCnt)
+        exam_new.setText(exam_new_cnt)
+        clear.setText(data.clearCnt)
+        clear_new.setText("+" + clear_new_cnt)
+        death.setText(data.deathCnt)
+        death_new.setText("+" + death_new_cnt)
+        state_dt.setText(data.stateDt)
+        state_time.setText(data.stateTime)
 
-            setLineChart(accumulate_chart,result)
-        }
-        override fun doInBackground(vararg params: Any?): List<ResponseElement> {
-            return readFeed(parsingData())
-        }
+        setLineChart(chart,result)
+    }
+    override fun doInBackground(vararg params: Any?): List<ResponseElement> {
+        return readFeed(parsingData())
     }
 }
